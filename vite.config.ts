086017da37d532c8b92dc21693e035e708aabd39
ponochovny <ponochovny@gmail.com>
2026-07-05 21:00:04 +0300

    feat: add deployment scripts and gh-pages dependency; update Vite config for base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/vue-image-modifier/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
